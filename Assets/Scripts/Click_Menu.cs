1b6ad22ca8a0b94fc31907dd59fc882886065ff1
JJW <jjw8584@gmail.com>
2024-05-09 00:51:02 +0900

    노트 에디터
    노트 생성 에디터 제작

--- Assets/Scripts/Click_Menu.cs
+++ Assets/Scripts/Click_Menu.cs
@@ -2,22 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//코드작성 : 권지수
 public class Click_Menu : MonoBehaviour
 {
     public GameObject Menu;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void btn_clicked()
     {
         Menu.SetActive(true);
--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//코드작성 : 지재원
 public class Note : MonoBehaviour
 {
     public int speed = 7;
