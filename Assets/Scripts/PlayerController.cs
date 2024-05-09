fc0d697bde948454cb2d05cf8c064a1ff870810e
JiSu <pinksun28@naver.com>
2024-05-10 02:28:06 +0900

    Revert "스프라이트 추가, 프리펩 생성, 코드 수정"
    This reverts commit a984b03dfbcabe4b2ce4190c2c8dffd75898156c.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     TimingManager theTimingManager;
 
-    Animator animator;
-
-    private void Awake()
+    void Start()
     {
-        animator = GetComponent<Animator>();
         theTimingManager = FindObjectOfType<TimingManager>();
     }
 
@@ -19,13 +16,6 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-            //theTimingManager.CheckTiming(); // 판정 체크
-            Motion();
-=======
->>>>>>> Stashed changes
 
             theTimingManager.CheckTiming_0(); // 판정 체크
         }
@@ -33,30 +23,6 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.J))
         {
             theTimingManager.CheckTiming_1(); // 판정 체크
-<<<<<<< Updated upstream
-=======
->>>>>>> 45f347536ee05bff237270e923c715d5f9f67051
->>>>>>> Stashed changes
         }
     }
-
-    void Motion() //노트 클릭시 캐릭터 모션                                     //코드작성 : 지재원
-    {                                                                           //..
-        int attackType = Random.Range(0, 2);                                    //..
-        switch (attackType)                                                     //..
-        {                                                                       //..
-            case 0:                                                             //..
-                animator.SetBool("isScratch", true);                            //..
-                break;                                                          //..
-            case 1:                                                             //..
-                animator.SetBool("isBodyShot", true);                           //..
-                break;                                                          //..
-        }                                                                       //..
-    }                                                                           //끝
-
-    //달리는 모션으로 돌아오기 위한 메소드
-    public void ReturnScratch() { animator.SetBool("isScratch", false); }       //코드작성 : 지재원
-    public void ReturnBodyshot() { animator.SetBool("isBodyShot", false); }     //..
-    public void ReturnHit() { animator.SetBool("isHit", false); }               //..
-    public void ReturnUppercut() { animator.SetBool("isUppercut", false); }     //끝
 }
