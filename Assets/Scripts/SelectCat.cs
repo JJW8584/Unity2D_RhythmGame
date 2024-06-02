be6bf9e59d4184b709d01789e3a72428c56ce175
JiSu <pinksun28@naver.com>
2024-06-02 21:53:08 +0900

    설정창 아이콘

--- Assets/Scripts/SelectCat.cs
+++ Assets/Scripts/SelectCat.cs
@@ -4,6 +4,13 @@ public class SelectCat : MonoBehaviour
 {
     public GameObject[] charSet;
 
+    Click_Menu Click_Menu;
+
+    private void Start()
+    {
+        Click_Menu = FindObjectOfType<Click_Menu>();
+    }
+
     private void OnEnable()
     {
         for (int i = 0; i < charSet.Length; i++)
@@ -12,5 +19,21 @@ public class SelectCat : MonoBehaviour
         }
 
         charSet[GameManager.instance.charType].SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            charSet[GameManager.instance.charType].SetActive(false);
+            GameManager.instance.charType = --GameManager.instance.charType < 0 ? 2 : GameManager.instance.charType;
+            charSet[GameManager.instance.charType].SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            charSet[GameManager.instance.charType].SetActive(false);
+            GameManager.instance.charType = ++GameManager.instance.charType > 2 ? 0 : GameManager.instance.charType;
+            charSet[GameManager.instance.charType].SetActive(true);
+        }
     }
 }
